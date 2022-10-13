9cf2665dc61dcfc4d130f3fe3499d41df5e59030
UcheAzubuko <azubukouche@yahoo.com>
2022-10-13 23:42:34 +0100

    Implementing local storage

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -11,4 +11,12 @@ class Task {
       required this.startTime,
       required this.endTime,
       required this.currentDate});
+
+  factory Task.fromMap(Map<String, dynamic> json) => Task(
+        id: json['id'],
+        name: json['name'],
+        endTime: json['startTime'],
+        startTime: json['startTime'],
+        currentDate: ['currentDate'],
+      );
 }
